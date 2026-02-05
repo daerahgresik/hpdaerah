88cb4dc92d2e15274d38569a590cc3a74a95af97
daerahgresik <daerahgresik@users.noreply.github.com>
2026-02-05 14:58:58 +0000

    deploy: 4aea39339ae9eac236fc1dd4266619546958d3c9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3389189249"
+    serviceWorkerVersion: "4042433322"
   }
 });
